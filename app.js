1be0a56c7760d12c02564aec826f24e4f20c6d25
Ben Jonathan <benjonathan0110@gmail.com>
2025-07-17 12:43:01 +0530

    final changes made

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const dbUrl = process.env.ATLASDB_URL
+const dbUrl = process.env.ATLASDB_URL;
+// const localUrl = "mongodb://localhost:27017/";
 
 const path = require("path");
 const methodOverride = require("method-override");
@@ -19,20 +20,20 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto : {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
-});
+// const store = MongoStore.create({
+//     mongoUrl: dbUrl,
+//     crypto : {
+//         secret: process.env.SECRET,
+//     },
+//     touchAfter: 24 * 3600,
+// });
 
-store.on("error", () => {
-    console.log("ERROR in MONGO SESSION STORE:", err);
-});
+// store.on("error", () => {
+//     console.log("ERROR in MONGO SESSION STORE:", err);
+// });
 
 const sessionOptions = {
-    store,
+    // store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
